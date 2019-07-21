549bd15c18b4187be88a13c979e486af3655e881
Rubén Soler <ruben.soler@exp360.com>
2019-07-21 17:16:23 +0200

    Feature test dependencies

--- mybingelist/src/App.js
+++ mybingelist/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+
 }
 
 export default App;
