b11bc860eb4d33c0ac446667136dc42b98526ced
Rubén Soler <ruben.soler@exp360.com>
2019-07-21 23:32:20 +0200

    Feature setup apollo graphql

--- mybingelist/src/container/movies/movies.js
+++ mybingelist/src/container/movies/movies.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import Header from '../../components/header/header';
 
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
 export class Movies extends Component {
 
   render() {
@@ -14,7 +17,35 @@ export class Movies extends Component {
     return (
       <div>
         <Header {...headerProps} />
-        Movies
+        <Query
+          query={gql`
+          query allMoviesByGenre {
+            allMovies(genre: "Science Fiction", limit: 10, offset:0) {
+              metadata {
+                limit
+                total
+                offset
+              }
+              data {
+                id
+                title
+                releaseYear
+                tagline
+              }
+            }
+          }
+    `}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+            return data.allMovies.data.map(({ id, title }) => (
+              <div key={id}>
+                <p>{title}</p>
+              </div>
+            ));
+          }}
+        </Query>
       </div>
     )
   }
--- mybingelist/src/index.js
+++ mybingelist/src/index.js
@@ -5,9 +5,16 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './createStore';
 
+import { ApolloProvider } from "react-apollo";
+
+import { client } from './apollo-client';
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>
+
   ,
   document.getElementById('root'));
