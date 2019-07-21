b58edcf803e7e02ff62ea81904ea6342f1ebef6f
Rubén Soler <ruben.soler@exp360.com>
2019-07-21 22:42:58 +0200

    Refactor remove unused code

--- mybingelist/src/App.js
+++ mybingelist/src/App.js
@@ -4,22 +4,13 @@ import Header from './components/header/header';
 import Movies from './container/movies/movies';
 import Favorites from './container/favorites/favorites';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.scss';
 
 export class App extends Component {
 
   render() {
-
-    const { location } = this.props;
-
-    const headerProps = {
-      showBackButton: location === '/',
-      showFavoritesButton: location !== '/',
-      title: 'MyBingeList'
-    }
-
     return (
       <Router>
         <main className="main">
