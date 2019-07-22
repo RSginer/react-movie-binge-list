e5f92b3856c44f2a13818edf0289509fbcc76693
Rubén Soler <ruben.soler@exp360.com>
2019-07-22 21:15:47 +0200

    Style movie component

--- mybingelist/src/components/movie/movie.js
+++ mybingelist/src/components/movie/movie.js
@@ -5,7 +5,22 @@ import './movie.scss';
 const Movie = (props) => {
   return (
     <div className="movie-container" data-test="movieComponent">
-      {props.title}
+      <div className="movie-container__wrap">
+        <div className="movie-thumbnail">
+          <img className="movie-thumbnail__img" src={props.poster.fullPath} alt={`Thubnail of movie ${props.title}`} />
+        </div>
+        <div className="movie-desc-wrap">
+          <div className="movie-desc-wrap__header">
+            {props.title}
+          </div>
+          <div className="movie-desc-wrap__body">
+            
+            </div>
+            <div className="movie-desc-wrap__footer">
+            
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
