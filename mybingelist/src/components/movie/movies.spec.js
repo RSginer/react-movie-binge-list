e861a83f405f0dc09f907bec504f05049b6e5daf
Rubén Soler <ruben.soler@exp360.com>
2019-07-22 18:20:59 +0200

    Test movie

---
+++ mybingelist/src/components/movie/movies.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Movie from './movie';
+import { findByTestAttr } from './../../../utils/test';
+import { shallow } from 'enzyme';
+
+describe('EmptyMessage Component', () => {
+  let component;
+
+  beforeEach(() => {
+
+    component = shallow(<Movie />);
+  });
+
+  it('Should render', () => {
+    const wrapper = findByTestAttr(component, 'movieComponent');
+    expect(wrapper.length).toBe(1);
+  });
+
+});
