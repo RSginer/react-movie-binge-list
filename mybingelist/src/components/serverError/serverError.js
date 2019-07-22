d5eefbefd5db1811c4957b67d949252d972a579e
Rubén Soler <ruben.soler@exp360.com>
2019-07-22 18:59:16 +0200

    Style cry iconn

--- mybingelist/src/components/serverError/serverError.js
+++ mybingelist/src/components/serverError/serverError.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import './serverError.scss';
+
 export const ServerError = (props) => {
   return (
-    <p data-test="serverErrorComponent"><span role="img" aria-label="cry">😭</span> Genre <u>{props.genre}</u> throws the following server error <i className="server-error-message" data-test="errorMessage">{props.message}</i></p>
+    <p className="serverError-wrap" data-test="serverErrorComponent"><span className="cry-icon" role="img" aria-label="cry">😭</span> Genre <u>{props.genre}</u> throws the following server error <i className="server-error-message" data-test="errorMessage">{props.message}</i></p>
   )
 }
 
