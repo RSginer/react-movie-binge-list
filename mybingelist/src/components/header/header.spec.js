50eabfaf15793869988f70efceebe80e8b25715c
Rubén Soler <ruben.soler@exp360.com>
2019-07-22 11:43:22 +0200

    Test header in multiple scenarios

--- mybingelist/src/components/header/header.spec.js
+++ mybingelist/src/components/header/header.spec.js
@@ -23,37 +23,115 @@ const findByTestAttr = (component, attr) => {
 
 
 describe('Header Component', () => {
-  let component;
 
-  beforeEach(() => {
-    const store = testStore({
-      header: {
-        showBackButton: true,
-        showFavoritesButton: true,
-        title: 'My Binge List'
-      }
+  describe('Renders all elements', () => {
+    let component;
+
+    beforeEach(() => {
+      const store = testStore({
+        header: {
+          showBackButton: true,
+          showFavoritesButton: true,
+          title: 'My Binge List'
+        }
+      })
+      component = shallow(<Header store={store} />).childAt(0).dive();
+    });
+
+    it('Should render', () => {
+      const wrapper = findByTestAttr(component, 'header');
+      expect(wrapper.length).toBe(1);
     })
-    component =  shallow(<Header store={store}/>).childAt(0).dive();
-  });
 
-  it('Should render', () => {
-    const wrapper = findByTestAttr(component, 'header');
-    expect(wrapper.length).toBe(1);
+    it('Should renders title', () => {
+      const text = findByTestAttr(component, 'title');
+      expect(text.text()).toBe('My Binge List')
+    })
+
+    it('Should renders back button', () => {
+      const backButton = findByTestAttr(component, 'backButton');
+      expect(backButton.length).toBe(1)
+    })
+
+    it('Should renders favorites button', () => {
+      const favoritesButton = findByTestAttr(component, 'favoritesButton');
+      expect(favoritesButton.length).toBe(1)
+    })
+
   })
 
-  it('Should renders title', () => {
-    const text = findByTestAttr(component, 'title');
-    expect(text.text()).toBe('My Binge List')
+
+  describe('Renders just title and backbutton', () => {
+    let component;
+
+    beforeEach(() => {
+      const store = testStore({
+        header: {
+          showBackButton: true,
+          showFavoritesButton: false,
+          title: 'My Binge List'
+        }
+      })
+      component = shallow(<Header store={store} />).childAt(0).dive();
+    });
+
+    it('Should render', () => {
+      const wrapper = findByTestAttr(component, 'header');
+      expect(wrapper.length).toBe(1);
+    })
+
+    it('Should renders title', () => {
+      const text = findByTestAttr(component, 'title');
+      expect(text.text()).toBe('My Binge List')
+    })
+
+    it('Should renders back button', () => {
+      const backButton = findByTestAttr(component, 'backButton');
+      expect(backButton.length).toBe(1)
+    })
+
+    it('Should renders favorites button', () => {
+      const favoritesButton = findByTestAttr(component, 'favoritesButton');
+      expect(favoritesButton.length).toBe(0)
+    })
+
   })
 
-  it('Should renders back button', () => {
-    const backButton = findByTestAttr(component, 'backButton');
-    expect(backButton.length).toBe(1)
+  describe('Renders just title and favoritesButton', () => {
+    let component;
+
+    beforeEach(() => {
+      const store = testStore({
+        header: {
+          showBackButton: false,
+          showFavoritesButton: true,
+          title: 'My Binge List'
+        }
+      })
+      component = shallow(<Header store={store} />).childAt(0).dive();
+    });
+
+    it('Should render', () => {
+      const wrapper = findByTestAttr(component, 'header');
+      expect(wrapper.length).toBe(1);
+    })
+
+    it('Should renders title', () => {
+      const text = findByTestAttr(component, 'title');
+      expect(text.text()).toBe('My Binge List')
+    })
+
+    it('Should renders back button', () => {
+      const backButton = findByTestAttr(component, 'backButton');
+      expect(backButton.length).toBe(0)
+    })
+
+    it('Should renders favorites button', () => {
+      const favoritesButton = findByTestAttr(component, 'favoritesButton');
+      expect(favoritesButton.length).toBe(1)
+    })
+
   })
 
-  it('Should renders favorites button', () => {
-    const favoritesButton = findByTestAttr(component, 'favoritesButton');
-    expect(favoritesButton.length).toBe(1)
-  })
 
 })
