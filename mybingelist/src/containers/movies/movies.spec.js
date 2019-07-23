feca7a3ef40aca88b3fb1e56aa55b6a0adf6572e
Rubén Soler <ruben.soler@exp360.com>
2019-07-23 17:58:02 +0200

    Test movies container

--- mybingelist/src/containers/movies/movies.spec.js
+++ mybingelist/src/containers/movies/movies.spec.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import Movies from './movies';
 import { mount } from 'enzyme';
+import { findByTestAttr } from './../../../utils/test';
 import { MockedProvider } from 'react-apollo/test-utils';
-import { testStore, findByTestAttr } from './../../../utils/test';
 
-import { GET_ALL_MOVIES } from './movies';
-
-import wait from 'waait';
-
-const filter = 'Science Fiction';
+import { GET_ALL_MOVIES } from '../../actions/index';
 
 const mocks = [
   {
     request: {
       query: GET_ALL_MOVIES,
       variables: {
-        genre: filter,
+        genre: 'War',
       },
     },
     result: {
@@ -32,21 +28,63 @@ const mocks = [
   },
 ];
 
+const moviesMock = [
+  {
+    id: 1,
+    title: 'Test title 1',
+    releaseYear: '2019',
+    overview: 'Test overview 1',
+    rating: 8,
+    genres: [
+      {
+        name: 'History',
+      },
+      {
+        name: 'Drama',
+      }
+    ],
+    poster: {
+      fullPath: 'https://image.tmdb.org/t/p/w342/noUp0XOqIcmgefRnRZa1nhtRvWO.jpg',
+    }
+  },
+  {
+    id: 2,
+    title: 'Test title 2',
+    releaseYear: '2019',
+    overview: 'Test overview 2',
+    rating: 8,
+    genres: [
+      {
+        name: 'History',
+      },
+      {
+        name: 'Drama',
+      }
+    ],
+    poster: {
+      fullPath: 'https://image.tmdb.org/t/p/w342/noUp0XOqIcmgefRnRZa1nhtRvWO.jpg',
+    }
+  }
+]
+
 describe('Movies Component', () => {
 
   describe('Fetch initial query and render loader', () => {
     let component;
 
     beforeEach(() => {
-      const store = testStore({
-        movies: {
-          filter: filter
-        }
-      })
+      const props = {
+        filter: undefined,
+        medianRating: undefined,
+        movies: [],
+        loading: true,
+        error: undefined,
+        favorites: [],
+        setupHeader: () => { }
+      }
       component = mount(
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Movies.WrappedComponent store={store} filter={filter} setupHeader={() => { }} />
-        </MockedProvider>);
+        <Movies.WrappedComponent {...props} />
+      );
     });
 
     it('Should render', () => {
@@ -61,19 +99,25 @@ describe('Movies Component', () => {
 
   });
 
-  describe('Show empty message if not filter exist', () => {
+
+  describe('Show error', () => {
     let component;
 
     beforeEach(() => {
-      const store = testStore({
-        movies: {
-          filter: undefined
-        }
-      })
+      const props = {
+        filter: undefined,
+        medianRating: undefined,
+        movies: [],
+        loading: false,
+        error: {
+          message: 'Error test'
+        },
+        favorites: [],
+        setupHeader: () => { }
+      }
       component = mount(
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Movies.WrappedComponent store={store} setupHeader={() => { }} />
-        </MockedProvider>);
+        <Movies.WrappedComponent {...props} />
+      );
     });
 
     it('Should render', () => {
@@ -81,9 +125,41 @@ describe('Movies Component', () => {
       expect(wrapper.length).toBe(1);
     });
 
-    it('Should render the empty message', async () => {
-      const message = findByTestAttr(component, 'emptyText');
-      expect(message.length).toBe(1);
+    it('Should render error component', async () => {
+      const error = findByTestAttr(component, 'serverErrorComponent');
+      expect(error.length).toBe(1);
+    });
+
+  });
+
+  describe('Show movies', () => {
+    let component;
+
+    beforeEach(() => {
+      const props = {
+        filter: undefined,
+        medianRating: undefined,
+        movies: moviesMock,
+        loading: false,
+        error: undefined,
+        favorites: [],
+        setupHeader: () => { }
+      }
+      component = mount(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Movies.WrappedComponent {...props} />
+        </MockedProvider>
+      );
+    });
+
+    it('Should render', () => {
+      const wrapper = findByTestAttr(component, 'moviesComponent');
+      expect(wrapper.length).toBe(1);
+    });
+
+    it('Should render error component', async () => {
+      const movies = findByTestAttr(component, 'movieComponent');
+      expect(movies.length).toBe(moviesMock.length);
     });
 
   });
