bf8a98bf7fbd1c0b408706306bf2e52e1067f36d
Rubén Soler <ruben.soler@exp360.com>
2019-07-23 22:19:10 +0200

    Remove unused action types

--- mybingelist/src/actions/types.js
+++ mybingelist/src/actions/types.js
@@ -1,7 +1,5 @@
 export const types = {
   SET_ROUTE: '[Header] setRoute',
-  CHANGE_GENRE: '[Movies] changeGenre',
-  CHANGE_MEDIAN_RATING: '[Movies] changeMedian',
   FETCH_MOVIES: '[Movies] fetchMovies',
   FETCH_MOVIES_SUCCESS: '[Movies] fetchMoviesSuccess',
   FETCH_MOVIES_ERROR: '[Movies] fetchMoviesError',
