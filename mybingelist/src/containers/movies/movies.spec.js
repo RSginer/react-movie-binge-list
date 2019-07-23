d10315e2fd6c157d8b48d9a58ff87943212be7f0
Rubén Soler <ruben.soler@exp360.com>
2019-07-23 21:42:21 +0200

    Fix Test movies gql

--- mybingelist/src/containers/movies/movies.spec.js
+++ mybingelist/src/containers/movies/movies.spec.js
@@ -4,7 +4,7 @@ import { mount } from 'enzyme';
 import { findByTestAttr } from './../../../utils/test';
 import { MockedProvider } from 'react-apollo/test-utils';
 
-import { GET_ALL_MOVIES } from '../../actions/index';
+import { GET_ALL_MOVIES } from '../../graphql';
 
 const moviesMock = [
   {
