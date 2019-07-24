9a3aa215eba994cf7b60b951c475cd344f364a70
Rubén Soler <ruben.soler@exp360.com>
2019-07-24 16:26:00 +0200

    Feature enable apollo-cache

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -2,20 +2,12 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 
-const defaultOptions = {
-  query: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'all',
-  },
-}
-
 const cache = new InMemoryCache({
   dataIdFromObject: object => object.id || null
 });
 
 export const client = new ApolloClient({
   link: createHttpLink({ uri: 'http://localhost:4000/graphql' }),
-  cache: cache,
-  defaultOptions
+  cache: cache
 });
 
