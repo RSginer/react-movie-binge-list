7b1370b25991826aa655114d5e986f1886bc17c5
Rubén Soler <ruben.soler@exp360.com>
2019-07-24 16:57:15 +0200

    Fix redux devtools extension check

--- src/createStore.js
+++ src/createStore.js
@@ -5,19 +5,27 @@ import createRootReducer from './reducers/index';
 import ReduxThunk from 'redux-thunk';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-
 export const history = createBrowserHistory()
+
+
+const enhancers = [
+  applyMiddleware(
+    routerMiddleware(history),
+    ReduxThunk
+  )
+]
+
+if (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__) {
+  enhancers.push(composeWithDevTools())
+}
+
+
 
 export default function configureStore() {
   const store = createStore(
     createRootReducer(history),
     compose(
-      applyMiddleware(
-        routerMiddleware(history),
-        ReduxThunk
-      ),
-    composeWithDevTools()
+      ...enhancers
     ),
   )
 
